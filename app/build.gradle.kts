57f8a96a7f96ecc4676034605b8f63319c36d0f8
sai somya <saisomyaranjan.pati@anviam.com>
2026-05-28 13:02:18 +0530

    initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,58 +1,108 @@
 plugins {
-    alias(libs.plugins.android.application)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
     namespace = "com.kharchamate.resumaster"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.kharchamate.resumaster"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner =
+            "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
+
             proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
+                getDefaultProguardFile(
+                    "proguard-android-optimize.txt"
+                ),
                 "proguard-rules.pro"
             )
         }
     }
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility =
+            JavaVersion.VERSION_17
+
+        targetCompatibility =
+            JavaVersion.VERSION_17
     }
-    
+
     buildFeatures {
         viewBinding = true
     }
 }
 
-dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+kotlin {
+    jvmToolchain(17)
+}
 
-    implementation(libs.androidx.lifecycle.viewmodel.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
-    implementation(libs.androidx.core.splashscreen)
-    implementation(libs.androidx.viewpager2)
+dependencies {
+
+    implementation(
+        "androidx.core:core-ktx:1.15.0"
+    )
+
+    implementation(
+        "androidx.appcompat:appcompat:1.7.0"
+    )
+
+    implementation(
+        "com.google.android.material:material:1.12.0"
+    )
+
+    implementation(
+        "androidx.activity:activity-ktx:1.10.1"
+    )
+
+    implementation(
+        "androidx.constraintlayout:constraintlayout:2.2.1"
+    )
+
+    implementation(
+        "androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7"
+    )
+
+    implementation(
+        "androidx.lifecycle:lifecycle-runtime-ktx:2.8.7"
+    )
+
+    implementation(
+        "androidx.navigation:navigation-fragment-ktx:2.8.9"
+    )
+
+    implementation(
+        "androidx.navigation:navigation-ui-ktx:2.8.9"
+    )
+
+    implementation(
+        "androidx.core:core-splashscreen:1.0.1"
+    )
+
+    implementation(
+        "androidx.viewpager2:viewpager2:1.1.0"
+    )
+
+    testImplementation(
+        "junit:junit:4.13.2"
+    )
+
+    androidTestImplementation(
+        "androidx.test.ext:junit:1.2.1"
+    )
+
+    androidTestImplementation(
+        "androidx.test.espresso:espresso-core:3.6.1"
+    )
 }
